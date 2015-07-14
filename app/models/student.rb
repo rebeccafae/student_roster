b34509cc605f6fc561216b159fc3b5461a6b6e1d
Rebecca <rfjackson11@gmail.com>
2015-07-14 11:03:31 -0600

    Add validations
    [#99046740]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ActiveRecord::Base
+  validates :first_name, :last_name, :email, :cell_phone, :work_phone, :home_phone, presence: true
+  validates :email, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :cell_phone, :work_phone, :home_phone, :format => {
+      with: (/(([0-9]{3})-([0-9]{3})-([0-9]{4}))\z/),
+      message: "should be in the format xxx-xxx-xxxx"
+      }
 end
